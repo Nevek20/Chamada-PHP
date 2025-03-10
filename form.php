5fa45d780770d873fb56bdffec40e177d7719e46
Matheus Guida <159961710+Nevek20@users.noreply.github.com>
2025-03-10 11:59:55 -0300

    Add files via upload

--- form.php
+++ form.php
@@ -1,30 +1,29 @@
-<html lang="pt-br">
-<style>
-    form{
-        padding-top: 100px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 8px;
-        
-    }
-</style>
-<body>
-    <main>
-        <form action="./aluno-cadastrar.php" method="POST">
-            <h2>Cadastrar alunos</h2>
-            <input type="hidden" placeholder="ID." name="id">
-            <input type="text" placeholder="Nome." name="nome">
-            <input type="text" placeholder="Telefone." name="telefone">
-            <input type="texty" placeholder="E-mail." name="email">
-            <input type="date" placeholder="Data de nascimento." name="nasc">
-            <div>
-                <label>Frequente?</label>
-                <input type="checkbox" name="frequente">
-            </div>
-            <input type="file" accept="image/*" name="img">
-            <input type="submit">
-        </form>
-    </main>
-</body> 
-</html>
+<html lang="pt-br"> <!-- idioma do site -->
+<style> /*abre a estilização */
+    form{ /* edita o formulário */
+        padding-top: 100px; /*bota um espaçamento encima */
+        display: flex; /*abre o flexbox */
+        flex-direction: column; /* coloca a direção em tipo coluna */
+        align-items: center; /* alinha no centro */
+        gap: 8px; /* faz um espaçamento */
+    } /* acabou a estilização*/
+</style> <!-- fecha o style -->
+<body> <!-- abre o body -->
+    <main> <!-- abre o main -->
+        <form action="./aluno-cadastrar.php" method="POST"> <!-- abre o formulario -->
+            <h2>Cadastrar alunos</h2> <!-- texto escrito cadastrar alunos -->
+            <input type="hidden" placeholder="ID." name="id"> <!-- coloca um input do ID escondido -->
+            <input type="text" placeholder="Nome." name="nome"> <!-- bota um input para o nome -->
+            <input type="text" placeholder="Telefone." name="telefone"> <!-- bota um input para o telefone -->
+            <input type="texty" placeholder="E-mail." name="email"> <!-- bota um input pro email -->
+            <input type="date" placeholder="Data de nascimento." name="nasc"> <!-- bota um input pra data de nascimento -->
+            <div> <!-- abre a div -->
+                <label>Frequente?</label> <!-- um texto perguntando se ele é frequente -->
+                <input type="checkbox" name="frequente"> <!-- coloca um checkbox -->
+            </div> <!-- fecha a div -->
+            <input type="file" accept="image/*" name="img"> <!-- coloca um input par a imagem -->
+            <input type="submit"> <!-- coloca um input pra enviar todas as informações -->
+        </form> <!-- fecha o formulario -->
+    </main> <!-- fecha o main -->
+</body> <!-- fecha o body -->
+</html> <!-- fecha o resto do html -->
--- index.php
+++ index.php
@@ -1,36 +1,36 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<?php
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> <!-- faz conexão com o boostrap -->
+<?php //abre o php
 
-$dsn = 'mysql:dbname=bd_chamadinha;host=localhost';
-$user = 'root';
-$password = '';
+$dsn = 'mysql:dbname=bd_chamadinha;host=localhost'; //conexão com o banco
+$user = 'root'; //usuario
+$password = ''; //senha
 
-$banco = new PDO($dsn, $user, $password);
+$banco = new PDO($dsn, $user, $password); //puxa tudo
 
-$select = 'SELECT * FROM tb_alunos';
-$resultado = $banco->query($select)->fetchALL();
+$select = 'SELECT * FROM tb_alunos'; //seleciona na tabela alunos tudo
+$resultado = $banco->query($select)->fetchALL(); //consulta e extrai o resultado
  
 //echo '<pre>';
 //var_dump ($resultado);
-?>
-<main class="container my-5">   
-    <table class="table table-striped">
-    <button><a href="./form.php">Criar novo aluno</a></button>
-        <tr>
-            <td>ID</td>
-            <td>Nome</td>
-            <td>Ações</td>
-        </tr>
-    <?php foreach($resultado as $lista) { ?>
-        <tr>
-            <td> <?= $lista ['Id'] ?> </td>
-            <td> <?php echo $lista ['Alunos'] ?> </td>
-            <td>
-                <a href="./ficha.php?id_alunos=<?= $lista['Id'] ?>" class="btn btn-primary">Abrir</a>
-                <a href="./form-editar.php?Id_aluno_alterar=<?= $lista['Id']?>" class="btn btn-warning">Editar</a> 
-                <a href="./aluno-deletar.php?Id=<?= $lista['Id']?>" class="btn btn-danger">Excluir</a> 
-            </td>
-        </tr>
-    <?php } ?>
-    </table>
-</main>
+?> <!-- acaba o php -->
+<main class="container my-5"> <!-- abre o main -->
+    <table class="table table-striped"> <!-- abre uma tabela -->
+    <button><a href="./form.php">Criar novo aluno</a></button> <!-- coloca um botão pra criar um novo aluno -->
+        <tr> <!-- abre uma linha -->
+            <td>ID</td> <!-- coloca uma coluna chamada ID -->
+            <td>Nome</td> <!-- coloca uma coluna chamada Nome -->
+            <td>Ações</td> <!-- coloca uma coluna chamada Ações -->
+        </tr> <!-- fecha a linha -->
+    <?php foreach($resultado as $lista) { ?> <!-- puxa do banco e coloca na lista -->
+        <tr> <!-- abre a linha -->
+            <td> <?= $lista ['Id'] ?> </td> <!-- bota o ID no ID -->
+            <td> <?php echo $lista ['Alunos'] ?> </td> <!-- bota o nome no alunos -->
+            <td> <!-- abre a coluna -->
+                <a href="./ficha.php?id_alunos=<?= $lista['Id'] ?>" class="btn btn-primary">Abrir</a> <!-- botao de abrir -->
+                <a href="./form-editar.php?Id_aluno_alterar=<?= $lista['Id']?>" class="btn btn-warning">Editar</a>  <!-- botao de editar -->
+                <a href="./aluno-deletar.php?Id=<?= $lista['Id']?>" class="btn btn-danger">Excluir</a>  <!-- botao de excluir-->
+            </td> <!-- fecha a coluna -->
+        </tr> <!-- fecha a linha -->
+    <?php } ?> <!-- fecha o php -->
+    </table> <!-- fecha a table -->
+</main> <!-- fecha o main -->
